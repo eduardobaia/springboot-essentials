2bb3b1e60c8f5091929c2c4e66ba173e081fe7ae
eduardobaia <eduardo_baia@msn.com>
2020-09-26 13:29:56 +0100

    implementing spring security

---
+++ src/main/java/pt/com/devdojo/awesome/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package pt.com.devdojo.awesome.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("eduardo").password("edu10").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER","ADMIN");
+    }
+}
